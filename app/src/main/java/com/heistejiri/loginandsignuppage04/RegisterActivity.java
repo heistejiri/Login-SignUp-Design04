3450a183396f43a87b45c89c194b8a7672b6f74c
Oghenetejiri Agoro <heistejiri@gmail.com>
2023-06-11 21:23:40 +0100

    Update RegisterActivity.java

--- app/src/main/java/com/heistejiri/loginandsignuppage04/RegisterActivity.java
+++ app/src/main/java/com/heistejiri/loginandsignuppage04/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.heistejiri.loginandsignuppage04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button1 click
+                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button1 click
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
